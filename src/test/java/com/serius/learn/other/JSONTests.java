34018e46c3f6b7c6f0611dceae1e4941320bbc64
cdtanwei1 <cdtanwei1@jd.com>
2018-10-12 09:42:45 +0800

    测试用例

--- src/test/java/com/serius/learn/other/JSONTests.java
+++ src/test/java/com/serius/learn/other/JSONTests.java
@@ -9,7 +9,7 @@ public class JSONTests {
 	
 	@Test
 	public void jsonTest() {
-		String tt = "[{\"attribute\":{\"education\":\"1,2,3,4\",\"userGender\":\"2\",\"ageGroup\":\"1,2,3,4,5,6\",\"cityId\":\"78\",\"provinceId\":\"2\",\"plus\":\"1\"},\"behavior\":{\"relationType\":\"1\",\"behavior4\":[{\"dimensionType\":\"1\",\"data\":[\"15126#15127#15132\",\"15126#15127#15132\"]}],\"behavior3\":[{\"dimensionType\":\"1\",\"data\":[\"15126#15127#15132\",\"15126#15127#15132\"]}],\"behavior2\":[{\"dimensionType\":\"1\",\"data\":[\"15126#15127#15132\",\"15126#15127#15132\"]}],\"behavior1\":[{\"dimensionType\":\"1\",\"data\":[\"15126#15127#15132\",\"15126#15127#15132\"]}]}},{\"attribute\":{\"education\":\"1\",\"userGender\":\"2\",\"ageGroup\":\"1\",\"cityId\":\"78\",\"provinceId\":\"2\",\"plus\":\"1\"},\"behavior\":{\"relationType\":\"1\",\"behavior4\":[{\"dimensionType\":\"1\",\"data\":[\"15126#15127#15132\",\"15126#15127#15132\"]},{\"dimensionType\":\"4\",\"brandName\":\"\"}],\"behavior3\":[{\"dimensionType\":\"1\",\"data\":[\"15126#15127#15132\",\"15126#15127#15132\"]},{\"dimensionType\":\"4\",\"brandName\":\"\"}],\"behavior2\":[{\"dimensionType\":\"1\",\"data\":[\"15126#15127#15132\",\"15126#15127#15132\"]},{\"dimensionType\":\"4\",\"brandName\":\"\"}],\"behavior1\":[{\"dimensionType\":\"1\",\"data\":[\"15126#15127#15132\",\"15126#15127#15132\"]},{\"dimensionType\":\"4\",\"brandName\":\"\"}]}}]";
+		String tt = "";
 		com.alibaba.fastjson.JSONArray array = JSON.parseArray(tt);
 		System.out.println(array.toJSONString());
 	}
